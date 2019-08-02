509fd57d96c0526a1fae5437a01aabd978333355
ahrenswett <ahrenswett@gmail.com>
2019-08-01 21:36:35 -0700

    lalalala

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 'use strict';
 
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 const superagent = require(`superagent`)
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 function Location(request, geoData) {
   this.search_query = request;
-  this.formatted_address = geoData.results[0].formatted_address;
-  this.latitude = Number(geoData.results[0].geometry.location.lat);
-  this.longitude = Number(geoData.results[0].geometry.location.lng);
+  this.formatted_address = geoData.body.results[0].formatted_address;
+  this.latitude = Number(geoData.body.results[0].geometry.location.lat);
+  this.longitude = Number(geoData.body.results[0].geometry.location.lng);
 }
 
 function Forecast(day) {
@@ -19,39 +19,43 @@ function Forecast(day) {
   this.time = new Date(day.time * 1000).toString().slice(0, 15);
 }
 
-function searchToLatLong(request, response) {
-   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
-   
-   return superagent.get(url)
-   .then(res => {
-    const location = new Location(request.query.data)
-    console.log('llama')
-    response.send(location)
+// function searchToLatLong(request, response) {
+//   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
+
+function serchToLatLong(query){
+  const url =`https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
+  return superagent.get(url)
+    .then(res => {
+      return new Location(query, res);
+
     })
 
 }
 
-app.use(express.static("./public"));
+app.use(express.static('./public'));
 app.use(cors());
 
+app.get('/location', (request, response) => {
+  serchToLatLong(request.query.data)//Serch box entry
+    .then(location => response.send(location));
+})
+// // app.get(`/location`, searchToLatLong);
 
-app.get(`/location`, searchToLatLong);
+// app.get('/location', (request, response) => {
+//   // try {
+//     const geo = require('./data/geo.json');
+//     console.log(request.query.data);
+//     const blob = new Location(request.query.data, geo);
+//     console.log(blob);
+//     response.send(blob);
+//   } catch (error) {
+//     handleError(error);
+//   }
+// });
 
-app.get("/location", (request, response) => {
+app.get('/weather', (request, response) => {
   try {
-    const geo = require("./data/geo.json");
-    console.log(request.query.data);
-    const blob = new Location(request.query.data, geo);
-    console.log(blob);
-    response.send(blob);
-  } catch (error) {
-    handleError(error);
-  }
-});
-
-app.get("/weather", (request, response) => {
-  try {
-    const weatherData = require("./data/darksky.json");
+    const weatherData = require('./data/darksky.json');
     const dailyWeather = Object.values(weatherData.daily.data);
     const blob = dailyWeather.map(day => new Forecast(day));
     console.log(blob);
@@ -59,20 +63,20 @@ app.get("/weather", (request, response) => {
   } catch (error) {
     handleError(error);
   }
- });
-
- 
- 
+});
 
 
-app.use("*", (request, response) =>
-  response.send("Sorry, that route does not exist.")
+
+
+
+app.use('*', (request, response) =>
+  response.send('Sorry, that route does not exist.')
 );
 
 function handleError(err, response) {
   console.error(err);
   if (response) {
-    response.status(500).send("Sorry, something went wrong here.");
+    response.status(500).send('Sorry, something went wrong here.');
   }
 }
 
